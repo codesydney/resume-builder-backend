7abfb8181ee75670c1cf40d63174408e36dff9bc
washington-shoji <shoji.washington@gmail.com>
2024-07-30 19:38:53 +1000

    - updated user schema entity and data type

--- src/schemas/user-schema.schema.ts
+++ src/schemas/user-schema.schema.ts
@@ -1,13 +1,12 @@
 import { sql } from 'drizzle-orm';
-import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
-import { UUID } from 'uuidv7';
+import { pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
-	id: serial('user_id').$type<UUID>().primaryKey(),
+	user_id: uuid('user_id').primaryKey(),
 	user_name: varchar('user_name', { length: 256 }).unique().notNull(),
 	email: varchar('email', { length: 256 }).unique().notNull(),
-	password: varchar('password', { length: 512 }).notNull(),
-	role: varchar('role', { length: 32 }).notNull(),
+	password: text('password').notNull(),
+	role: text('role').$type<'USER' | 'SUPER_ADMIN'>().notNull().default('USER'),
 	created_at: timestamp('created_at', {
 		precision: 6,
 		withTimezone: true,
@@ -18,5 +17,22 @@ export const users = pgTable('users', {
 	}),
 });
 
+/**
+ * Data Types
+ * Bellow are data types for different events i.e Request, Response etc
+ */
+
 export type UserEntity = typeof users.$inferSelect; // return type when queried
 export type NewUserEntity = typeof users.$inferInsert; // insert type
+
+export type UserRole = 'USER' | 'SUPER_ADMIN';
+
+export type UserRequest = Omit<
+	NewUserEntity,
+	'user_id' | 'created_at' | 'updated_at' | 'role'
+>;
+
+export type UserResponse = Omit<
+	NewUserEntity,
+	'password' | 'created_at' | 'updated_at' | 'role'
+>;
