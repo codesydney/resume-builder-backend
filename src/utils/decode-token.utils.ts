274302ab30dd802592ab8901b2e9d4c66f33af2b
washington-shoji <shoji.washington@gmail.com>
2024-07-31 18:57:18 +1000

    - added decode token helper function

--- src/utils/decode-token.utils.ts
+++ src/utils/decode-token.utils.ts
@@ -9,6 +9,22 @@ export type TokenPayload = {
 	exp: number;
 };
 
+//TODO: This should be a generic helper function, refactor later
+export function decodeAccessToken(token: string | undefined) {
+	if (!token) return;
+
+	// TODO: decide if set up JWT_SECRET(s) as arg param
+	const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+
+	if (decoded) {
+		const arrayToken = token.split('.');
+		const tokenPayload: TokenPayload = JSON.parse(atob(arrayToken[1]));
+
+		return tokenPayload;
+	}
+}
+
+//TODO: This should be a generic helper function, refactor later
 export function validateAndDecodeRefreshToken(
 	refreshToken: string | undefined
 ): TokenPayload | undefined {
